b9216929e03d98bff52f56bf42e506dd73cf16c3
Farhana Yasmin <69351474+Farhana1176@users.noreply.github.com>
2025-04-16 19:48:07 +0300

    Successfully Installed "Ui Shadcn"

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import { Button } from './components/ui/button'
 
 const App = () => {
   return (
-    <div className=' text-3xl font-bold text-blue-300 text-center'>App</div>
+    <div className='flex flex-col items-center justify-center h-screen'>
+      <h1 className=' text-3xl font-bold text-blue-800 '>Welcome to React News Project</h1>
+      <Button className=' bg-slate-500 text-white' variant='ghost'>Click Me</Button>
+    </div>
   )
 }
 
